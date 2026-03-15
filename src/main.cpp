f666f239d695d05add65d4f848a508877b17a029
007 <2974689801@qq.com>
2026-03-15 20:40:12 +0800

    Add logic of surface format and QPA.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 ﻿#include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
 #include <QStyleFactory>
+#include <QSurfaceFormat>
 int main(int argc, char *argv[])
 {
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
     QApplication a(argc, argv);
+    // 验证实际使用的平台
+    qDebug() << "Platform name:" << QGuiApplication::platformName();
     a.setStyle(QStyleFactory::create("fusion"));
     MainWindow w;
     w.show();
